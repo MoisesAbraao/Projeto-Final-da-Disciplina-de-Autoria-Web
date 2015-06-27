42d30c9427dac96eb70f7f88df81468f778e4d23
MoisesAbraao <moisesdirt_bmx@hotmail.com>
2015-06-27 12:22:15 -0300

    fist commit

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function enviar(){
+	if(document.getElementById('text1').value!="") {
+	document.getElementById('text1').value="";
+	document.getElementById('text2').value="";
+	document.getElementById('text3').value="";
+	alert("Enviado com sucesso!");
+	
+}
+}
